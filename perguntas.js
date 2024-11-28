ecb9339bf28b3453e1869e0c05e57a084fc340a8
geovannaglendaa <00001114558862sp@al.educacao.sp.gov.br>
2024-11-28 16:38:19 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Arte',
+    'Quem pintou a Mona Lisa?',
+    'Leonardo da vinci'
+)
+
+criaCartao(
+    'Astronomia',
+    'Quantos planetas existem no Sistema Solar?',
+    '8 planetas'
+)
+
+criaCartao(
+    'Geografia',
+    'Onde esta localizado o Cristo Redentor?',
+    'No Rio de Janeiro'
+)
+
+criaCartao(
+    'História',
+    'Em que ano acabou a Segunda Guerra Mundial?',
+    '1945'
+)
